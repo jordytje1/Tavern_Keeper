f9ca99a56990ae7b39080a932d79c27a2709a4cc
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-06-22 21:24:54 +0800

    Added bot info Command

---
+++ commands/info/botinfo.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-mixed-spaces-and-tabs */
+const { MessageEmbed } = require('discord.js');
+const { formatBytes } = require('../../functions.js');
+const { parseDur } = require('../../functions.js');
+const moment = require('moment');
+const prefix = process.env.prefix;
+const os = require('os');
+const cpuStat = require('cpu-stat');
+
+module.exports = {
+	name: 'botinfo',
+	category: 'info',
+	description: 'Displays information about the bot.',
+	aliases: ['bot'],
+	usage: `${prefix}botinfo`,
+	run: async (client, message, args) => {
+		cpuStat.usagePercent(function(error, percent, seconds) {
+			if(error) {
+				return console.error(error);
+			}
+			const core = os.cpus()[0];
+			const cores = os.cpus().length;
+
+			const embed = new MessageEmbed()
+				.setDescription(`**Bot information for ${client.user.username}**`)
+				.setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 512 }))
+				.setColor(client.displayHexColor || 'BLUE')
+				.addField('General Statistics:', [
+					`**❯ Server:** ${client.guilds.cache.size.toLocaleString()}`,
+					`**❯ Users:** ${client.users.cache.size.toLocaleString()}`,
+					`**❯ Channel:** ${client.channels.cache.size.toLocaleString()}`,
+					`**❯ Commands:** ${client.commands.size}`,
+					`**❯ Creation Date:** ${moment(client.user.createdTimestamp).format('Do MMMM YYYY HH:mm')}`,
+					`**❯ Uptime:** ${parseDur(client.uptime)}`,
+					'\u200b',
+				])
+				.addField('System Statistics:', [
+					`**❯ Node.js Version:** ${process.version}`,
+					`**❯ Platform:** ${os.platform}`,
+					'**❯ CPU:** ',
+					`\u3000 - Model: ${core.model}`,
+					`\u3000 - Cores: ${cores}`,
+					`\u3000 - Speed: ${core.speed}MHz`,
+					`\u3000 - Usage: ${percent.toFixed(2)}%`,
+					'**❯ Memory:** ',
+					`\u3000 - Total: ${formatBytes(process.memoryUsage().heapTotal)}`,
+					`\u3000 - Used: ${formatBytes(process.memoryUsage().heapUsed)}`,
+					'\u200b',
+				]);
+			message.channel.send(embed);
+		});
+	},
+};
--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 module.exports = {
 	getMember: function(message, toFind = '') {
 		toFind = toFind.toLowerCase();
@@ -32,5 +33,38 @@ module.exports = {
 		return message
 			.awaitReactions(filter, { max: 1, time: time })
 			.then(collected => collected.first() && collected.first().emoji.name);
+	},
+
+	formatBytes: function(a, b) {
+		if (a == 0) return '0 Bytes';
+		const c = 1024,
+			d = b || 2,
+			e = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+			f = Math.floor(Math.log(a) / Math.log(c));
+
+		return parseFloat((a / Math.pow(c, f)).toFixed(d)) + ' ' + e[f];
+	},
+
+	parseDur: function(ms) {
+		let seconds = ms / 1000,
+			days = parseInt(seconds / 86400);
+		seconds = seconds % 86400;
+
+		const hours = parseInt(seconds / 3600);
+		seconds = seconds % 3600;
+
+		const minutes = parseInt(seconds / 60);
+		seconds = parseInt(seconds % 60);
+
+		if (days) {
+			return `${days} day, ${hours} hours, ${minutes} minutes`;
+		}
+		else if (hours) {
+			return `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+		}
+		else if (minutes) {
+			return `${minutes} minutes, ${seconds} seconds`;
+		}
+		return `${seconds} second(s)`;
 	},
 };
