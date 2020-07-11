fd38736342990b16ea9b99e6f2dd4cc39928dc86
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-07-11 10:18:37 +0000

    Edited Avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -12,7 +12,7 @@ module.exports = {
 	run: async (client, message, args) => {
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]) || message.author;
 		const embed = new MessageEmbed()
-			.setTitle(`${member.user.username}'s avatar`)
+			.setTitle(`${member.user.name}'s avatar`)
 			.setURL(member.user.displayAvatarURL({ dynamic: true }))
 			.setImage(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
 			.setColor('BLUE');
