df940eee7368c8ecfe1dd3b43db1f067ad1058ac
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-07-11 10:17:40 +0000

    Edited Avatar Command

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -10,7 +10,7 @@ module.exports = {
 	usage: `${prefix}avatar [@user | userid]`,
 	guildOnly: true,
 	run: async (client, message, args) => {
-		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]);
+		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]) || message.author;
 		const embed = new MessageEmbed()
 			.setTitle(`${member.user.username}'s avatar`)
 			.setURL(member.user.displayAvatarURL({ dynamic: true }))
