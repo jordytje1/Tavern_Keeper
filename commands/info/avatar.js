e247ff0223c155a68db71370053335145fcb6857
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-07-11 10:20:21 +0000

    Edited Avatar Command

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -12,9 +12,9 @@ module.exports = {
 	run: async (client, message, args) => {
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]) || message.author;
 		const embed = new MessageEmbed()
-			.setTitle(`${member.user.name}'s avatar`)
+			.setTitle(`${member.username}'s avatar`)
 			.setURL(member.user.displayAvatarURL({ dynamic: true }))
-			.setImage(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
+			.setImage(member.user.displayAvatarURL({ dynamic: true }))
 			.setColor('BLUE');
 		message.channel.send(embed);
 	},
