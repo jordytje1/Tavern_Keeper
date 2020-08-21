b5b18485ad48ceda85fe4d4ab37a930a4979031a
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-08-21 07:48:36 +0000

    Updated Stuff

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,30 +1,26 @@
 const prefix = process.env.BOT_PREFIX;
 
 module.exports = async (client, message) => {
-	if (message.author.bot) return;
-	if (!message.guild) return;
+  if (message.author.bot) return;
+  if (!message.guild) return;
 
-	if(message.content === `<@${client.user.id}>`) {
-		return message.channel.send(`My current prefix for this guild is \`${prefix}\``);
-	}
+  if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
+    message.channel.send(`My current prefix for this guild is \`${prefix}\``);
+  }
 
-	if(message.content === `<@!${client.user.id}>`) {
-		return message.channel.send(`My current prefix for this guild is \`${prefix}\``);
-	}
+  if (!message.content.startsWith(prefix)) return;
 
-	if (!message.content.startsWith(prefix)) return;
+  if (!message.member) message.member = await message.guild.fetchMember(message);
 
-	if (!message.member) message.member = await message.guild.fetchMember(message);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const cmd = args.shift().toLowerCase();
+  if (cmd.length === 0) return;
 
-	if (cmd.length === 0) return;
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-	let command = client.commands.get(cmd);
-	if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-	if (command) {
-		command.run(client, message, args);
-	}
+  if (command) {
+    command.run(client, message, args);
+  }
 };
