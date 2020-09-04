4e63d49f2c0624cf75187ae85bd8d5b8ca5b9cbd
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-04 20:39:59 +0200

    Update suggest.js

--- commands/misc/suggest.js
+++ commands/misc/suggest.js
@@ -12,7 +12,7 @@ module.exports = {
 		const text = args.slice().join(' ');
 		if(!text) {
 			return message.channel.send(
-				'<❌725270799124004934> Please provide valid text',
+				'<:x:725270799124004934> Please provide valid text',
 			);
 		}
 		const channel = client.channels.cache.get('734719742958633041');
@@ -21,7 +21,7 @@ module.exports = {
 			`\`[${moment(message.createdTimestamp).format('HH:mm:ss')}]\` ❗ **${message.author.username}**#${message.author.discriminator} (ID: ${message.author.id}) has made a suggestion in **${message.guild.name}** (ID: ${message.guild.id}).\n\`[Suggestion]\` ${text}`,
 		);
 		await message.channel.send(
-			'<✔️725270799098970112> Successfully made the suggestion.',
+			'<:white_check_mark:725270799098970112> Successfully made the suggestion.',
 		).then(message.delete());
 	},
 };
