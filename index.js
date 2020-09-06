01830db2f76fb057c4b93068c08234d401627baa
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-06 19:40:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ client.category = new Collection();
 	require(`./handlers/${handler}`)(client);
 });
 
-
+ client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","lol"));
+    });
 
 keepAlive();
 client.login(process.env.BOT_TOKEN);
