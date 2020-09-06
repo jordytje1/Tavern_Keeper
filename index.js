c3b2280cc048d5fa0dae85cce51d9c1f05e2f44a
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-06 19:20:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,23 +14,6 @@ client.category = new Collection();
 });
 
 
-client.on('guildMemberAdd', member =>{
-
-    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-
-    if(!channel) return;
-
-    let embed = new MessageEmbed()
-        .setColor(pink)
-        .setAuthor('IdealBot', 'https://hypixel.net/attachments/ideal-png.1417277/', 'https://hypixel.net/threads/ideal-ideal-%E2%9D%96-level-52-%E2%9D%96-sweaty-skyblock-guild-%E2%9D%96-top-10-sb-guild-%E2%9D%96-splashes-%E2%9D%96-events-%E2%9D%96-recruiting.2500755/')
-        .setTitle('Welcome!')
-        .setDescription(`${member} just joined the discord! Make sure to read #rules!`)
-        .setThumbnail(message.user.avatarURL)
-        .setFooter('Note: The maximum amount of answers is 9.')
-        .setTimestamp();
-
-    channel.send(embed);
-});
 
 
 
