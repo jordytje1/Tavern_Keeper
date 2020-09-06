cae9f74938e4a6176cadce0c5cde945ad6b802bd
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-06 18:49:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,39 @@ client.snipes = new Map();
 	require(`./handlers/${handler}`)(client);
 });
 
+
+
+
+client.on('guildMemberAdd', member => {
+  let channel = member.guild.channels.find('name', 'channel.name');
+  let memberavatar = member.user.avatarURL
+      if (!channel) return;
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(memberavatar)
+      .addField('Welcome', `${member}`)
+      .addField(':family_mwgb: | Actually there is ', `${member.guild.memberCount} people`)
+      .addField("Name", `<@` + `${member.id}` + `>`, true)
+      .addField('Server', `${member.guild.name}`, true )
+      .setFooter(`**${member.guild.name}**`)
+      .setTimestamp()
+
+      channel.sendEmbed(embed);
+});
+
+client.on('guildMemberAdd', member => {
+
+  console.log(`${member}`, "has arrived" + `${member.guild.name}`)
+
+});
+
+
+
+
+
+
+
+
+
 keepAlive();
 client.login(process.env.BOT_TOKEN);
