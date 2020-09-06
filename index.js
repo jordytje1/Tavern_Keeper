b9be3c8930bc59daed490253efa144c4918c6ec1
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-06 16:09:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
-const { Client, Collection, Discord } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const keepAlive = require('./server');
-prefix = "!",
 const client = new Client({
 	disableEveryone: true,
 });
@@ -14,39 +13,6 @@ client.category = new Collection();
 	require(`./handlers/${handler}`)(client);
 });
 
-var emojiname = ["", ""],
-    rolename = ["", ""];
-
-
-
-
-client.on("message", e => {
-    if (e.content.startsWith(prefix + "reaction")) {
-        if (!e.channel.guild) return;
-        for (let o in emojiname) {
-            var n = [e.guild.emojis.find(e => e.name == emojiname[o])];
-            for (let o in n) e.react(n[o])
-        }
-    }
-});
-
-client.on("messageReactionAdd", (e, n) => {
-    if (n && !n.bot && e.message.channel.guild)
-        for (let o in emojiname)
-            if (e.emoji.name == emojiname[o]) {
-                let i = e.message.guild.roles.find(e => e.name == rolename[o]);
-                e.message.guild.member(n).addRole(i).catch(console.error)
-            }
-});
-
-client.on("messageReactionRemove", (e, n) => {
-    if (n && !n.bot && e.message.channel.guild)
-        for (let o in emojiname)
-            if (e.emoji.name == emojiname[o]) {
-                let i = e.message.guild.roles.find(e => e.name == rolename[o]);
-                e.message.guild.member(n).removeRole(i).catch(console.error)
-            }
-});
 
 
 
