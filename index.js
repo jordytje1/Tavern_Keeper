9327dc9cf26811b602c1065f8c7592a949fd6789
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-06 19:11:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,28 +14,25 @@ client.category = new Collection();
 });
 
 
-client.on('guildMemberAdd', member => {
-  let channel = member.guild.channels.find('name', 'channel.name');
-  let memberavatar = member.user.avatarURL
-      if (!channel) return;
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setThumbnail(memberavatar)
-      .addField('Welcome', `${member}`)
-      .addField(':family_mwgb: | Actually there is ', `${member.guild.memberCount} people`)
-      .addField("Name", `<@` + `${member.id}` + `>`, true)
-      .addField('Server', `${member.guild.name}`, true )
-      .setFooter(`**${member.guild.name}**`)
-      .setTimestamp()
+client.on('guildMemberAdd', member =>{
 
-      channel.sendEmbed(embed);
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+
+    if(!channel) return;
+
+    const welcomeEmbed = new Discord.MessageEmbed()
+        .setColor(pink)
+        .setAuthor('IdealBot', 'https://hypixel.net/attachments/ideal-png.1417277/', 'https://hypixel.net/threads/ideal-ideal-%E2%9D%96-level-52-%E2%9D%96-sweaty-skyblock-guild-%E2%9D%96-top-10-sb-guild-%E2%9D%96-splashes-%E2%9D%96-events-%E2%9D%96-recruiting.2500755/')
+        .setTitle('Welcome!')
+        .setDescription(`${member} just joined the discord! Make sure to read #rules!`)
+        .setThumbnail(message.user.avatarURL)
+        .setFooter('Note: The maximum amount of answers is 9.')
+        .setTimestamp();
+
+    channel.send(welcomeEmbed);
 });
 
-client.on('guildMemberAdd', member => {
 
-  console.log(`${member}`, "has arrived" + `${member.guild.name}`)
-
-});
 
 keepAlive();
 client.login(process.env.BOT_TOKEN);
