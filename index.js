fd3de541d8a3c0119b3213b9fde4ce1777e215fc
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-08 20:20:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,18 +41,6 @@ client.on("guildMemberRemove", member => {
 
 
 
-client.on('message', (message) => {
-       if (message.channel.id === "752211512248107175"){
-           message.delete()
-
-});
-		
-        }
-
-});
-
-
-
 
 
 
