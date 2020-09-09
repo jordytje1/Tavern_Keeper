94c27459060d86920ea1471953504c75564d116e
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-09 19:19:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const { Client, Collection } = require('discord.js');
 const keepAlive = require('./server');
-
+const prefix = "!";
+const unverify_role = 'Your Unverified RoleID Here';
+const verify_role = '752905551318351904';
 const client = new Client({
 	disableEveryone: true,
 });
