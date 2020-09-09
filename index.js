204b842454d8fc86d307c281b3d79255eef9e8aa
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-09 18:26:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ client.on("guildMemberAdd", member => {
   member.roles.add("752585847534125096"); // Member role.
 })
 
-
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  // Ignore dms
+  if (message.channel.type == "dm");
+  member.roles.add("752585847534125096"); // Member role.
+})
 
 client.on("guildMemberRemove", member => {
   if (member.guild.id !== "752211511996317827") return;
