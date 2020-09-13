39af770f5d030281485db14ea79b0c7ee321f816
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-13 16:32:41 +0200

    Create 9.js

---
+++ commands/misc/9.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require("discord.js")
+
+
+module.exports = {
+  name: "report",
+  usage: "report <message>",
+  description: "Send your report",
+  category: "main",
+  guildonly: "false",
+  run: async (client, message, args) => {
+    
+    if(!args.length) {
+      return message.channel.send("Please Give the report")
+    }
+    
+    const channel = client.channels.cache.get('752211513401671763');
+    
+    
+    if(!channel) {
+      return message.channel.send("there is no channel with name - suggestions")
+    }
+                                                    
+    
+    let embed = new MessageEmbed()
+    .setTitle('Report')
+    .setThumbnail(message.author.avatarURL())
+    .setColor("#ff2509")
+    .setDescription(args.join(" "))
+    .setFooter(`Requested by [${message.author.tag}] in [${message.guild.name}] guild ID : [${message.guild.id}]`)
+    .setTimestamp()   
+    
+    
+    channel.send(embed).then(m => {
+      m.react("✅")
+      m.react("❌")
+    })
+    
+
+    
+    message.channel.send("Sended Your report to " + channel),
+    user.send('<content>');
+    
+  }
+}
