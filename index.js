22cc55a814ef5c2328fca01ba8f7ef67ad73ecc2
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-14 20:09:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ client.on("message", (message) => {
         message.channel.send("This message has been send to the staff! :incoming_envelope:").then(msg => msg.delete(3000))
         if (message.content.startsWith(config.PREFIX)) return
         if (args.length > 1024) return message.reply("Your message content too many characters (1024 Limit) :/") 
-        var embed = new Discord.RichEmbed()
+        const embed = new Discord.RichEmbed()
             .setColor(config.ORANGE)
             .setAuthor("New Message", "https://cdn.discordapp.com/attachments/502649544622735362/520740243133956138/receive.png")
             .addBlankField(true)
