7993e31c8a65c209f21317fcb5b5fcb6b0dc259e
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-14 20:29:38 +0200

    Update reply.js

--- commands/misc/reply.js
+++ commands/misc/reply.js
@@ -24,8 +24,10 @@ const replyMessage = argse;
 const embed = new Discord.MessageEmbed()
   .setTitle(`Message Sent!`)
   .setDescription(`Successfully send the message to <@${user.id}> (${userr.tag})`)
+  .addfield("want to type a message back?? try a guild where the bot is in and send !message @user [message]"
   .addField("Message:", replyMessage)
   .setColor("62c95d")
+  .setTimestamp()
 message.author.send(embed)
 
 const replyEmbed = new Discord.MessageEmbed()
@@ -33,6 +35,7 @@ const replyEmbed = new Discord.MessageEmbed()
   .setDescription(`You have recieved a reply from ${message.author.tag}`)
   .addField("Message:", replyMessage)
   .setColor("4f8edb")
+  .setTimestamp()
 user.send(replyEmbed)
 
   } else{
