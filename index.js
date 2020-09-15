26226f7494785006d9360340fec8f9af063bf172
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 15:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,32 +63,6 @@ client.on('messageReactionRemove', async (reaction, user) => {
 
 
 
-client.on('message', message => {
-      let args = message.content.substring(prefix.length).split(" ");
-
-      if (message.content.startsWith(`${prefix}lockdown`)) {
-        if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply('You can\'t use this command!')
-        const channel = message.guild.channels.cache.filter(ch => ch.type !== 'category');
-        if (args[1] === 'on') {
-                channel.updateOverwrite(message.guild.roles.everyone, {
-                    SEND_MESSAGES: false
-                }).then(() => {
-                    channel.setName(channel.name += `🔒`)
-                })
-            })
-            return message.channel.send('Locked all channels');
-        } else if (args[1] === 'off') {
-                channel.updateOverwrite(message.guild.roles.everyone, {
-                    SEND_MESSAGES: true
-                }).then(() => {
-                    channel.setName(channel.name.replace('🔒', ''))
-                })
-            })
-            return message.channel.send('Unlocked all channels')
-        }
-    }
-});
-
 
 
 
