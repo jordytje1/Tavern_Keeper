798b8fe46bfe2ee0ee5292281d92755e7fb5a9d0
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 16:19:34 +0200

    Update channel.js

--- commands/moderation/channel.js
+++ commands/moderation/channel.js
@@ -42,7 +42,17 @@ module.exports = {
 
   
 
-  message.guild.channels.create(args.slice(0).join(" "), {type: 'text'});
+  message.guild.channels.create(args.slice(0).join(" "), {type: 'text',
+			      permissionOverwrites: [{
+			          allow: "VIEW_CHANNEL",
+			          id: message.author.id
+		        },
+		        {
+				  deny: "VIEW_CHANNEL",
+				  id: message.guild.id
+			}
+		   ]
+	})
 
   message.channel.send("Channel successfully created!");
 
