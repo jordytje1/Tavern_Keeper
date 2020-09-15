d748badfe654c6f09303cbef57bbd6e32ce86ff0
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 14:04:56 +0200

    Update reply.js

--- commands/misc/reply.js
+++ commands/misc/reply.js
@@ -15,7 +15,7 @@ const userr = message.mentions.users.first();
 const user = message.guild.member(userr);
       
 //if (user.roles.has('746858184504770651')) {
-if(message.member.roles.cache.some(role => role.name === '『💛』『support』')){
+if (message.channel.type) === 'dm')){
 
 const argse = message.content.slice(`m+reply ${userr}`.length).split( );
       
