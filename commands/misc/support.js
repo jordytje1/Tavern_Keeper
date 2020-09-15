c31ea3398da48bdfca96ad01af4c23a0eae2b984
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 13:53:57 +0200

    Create support.js

---
+++ commands/misc/support.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+//packges or const here!
+module.exports = {
+    name: "support",//aka the folder name without js
+    aliases: ["support"],
+    category: "support",//this is what ever ever dir you are in (dev, info, moderation, music)
+    description: "message a user",
+    usage: "support {user <@id>} {msg content}",
+    run: async (client, message, args) => {
+//your code goes here!
+      
+//const args = message.content.slice(`m+reply `.length).split( );
+const memberr = args[1];
+const userr = message.mentions.users.first();
+const user = message.guild.member(userr);
+      
+//if (user.roles.has('746858184504770651')) {
+if(message.member.roles.cache.some(role => role.name === '『💛』『support』')){
+
+const argse = message.content.slice(`m+reply ${userr}`.length).split( );
+      
+const supportMessage = argse;
+      
+const embed = new Discord.MessageEmbed()
+  .setTitle(`Message Sent!`)
+  .setDescription(`Successfully send the message to <@${user.id}> (${userr.tag})`)
+  .addField("Message:", supportMessage)
+  .setColor("62c95d")
+  .setTimestamp()
+message.author.send(embed)
+
+const replyEmbed = new Discord.MessageEmbed()
+  .setTitle(`New Message!`)
+  .setDescription(`**You have recieved a message from ${message.author.tag}**`)
+  .addField("Message:", supportMessage)
+  .setColor("4f8edb")
+  .setTimestamp()
+user.send(replyEmbed)
+
+  } else{
+
+  message.reply(`Error Code 403 ||| Solution: Please gain the role "Modmail Access" to run this command!`)
+    
+  }
+      
+//end code
+  }
+}
