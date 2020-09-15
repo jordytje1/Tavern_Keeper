d4e3882441aaaa81f1c5b876438c12903faf474d
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 14:28:26 +0200

    Update and rename reply.js to message.js

--- commands/misc/message.js
+++ commands/misc/message.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 //packges or const here!
 module.exports = {
-    name: "reply",//aka the folder name without js
+    name: "message",//aka the folder name without js
     aliases: ["message"],
     category: "message",//this is what ever ever dir you are in (dev, info, moderation, music)
     description: "message a user",
@@ -17,29 +17,29 @@ const user = message.guild.member(userr);
 //if (user.roles.has('746858184504770651')) {
 if(message.member.roles.cache.some(role => role.name === '『💛』『support』')){
 
-const argse = message.content.slice(`m+reply ${userr}`.length).split( );
+const argse = message.content.slice(`m+message ${userr}`.length).split( );
       
-const replyMessage = argse;
+const messageMessage = argse;
       
 const embed = new Discord.MessageEmbed()
   .setTitle(`Message Sent!`)
   .setDescription(`Successfully send the message to <@${user.id}> (${userr.tag})`)
-  .addField("Message:", replyMessage)
+  .addField("Message:", messageMessage)
   .setColor("62c95d")
   .setTimestamp()
 message.author.send(embed)
 
-const replyEmbed = new Discord.MessageEmbed()
+const messageEmbed = new Discord.MessageEmbed()
   .setTitle(`New Message!`)
   .setDescription(`**You have recieved a message from ${message.author.tag}**`)
-  .addField("Message:", replyMessage)
+  .addField("Message:", messageMessage)
   .setColor("4f8edb")
   .setTimestamp()
-user.send(replyEmbed)
+user.send(messageEmbed)
 
   } else{
 
-  message.reply(`Error Code 403 ||| Solution: Please gain the role "Modmail Access" to run this command!`)
+  message.message(`Error Code 403 ||| Solution: Please gain the role "Modmail Access" to run this command!`)
     
   }
       
