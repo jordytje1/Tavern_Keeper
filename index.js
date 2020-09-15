f3a8376a766e82286ca47d92ee49a037225eff76
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-15 15:14:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,9 +68,8 @@ client.on('message', message => {
 
       if (message.content.startsWith(`${prefix}lockdown`)) {
         if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply('You can\'t use this command!')
-        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
-        if (args[1] === 'on') {
-            channels(channel => {
+        const channel = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[1] === 'on') => {
                 channel.updateOverwrite(message.guild.roles.everyone, {
                     SEND_MESSAGES: false
                 }).then(() => {
@@ -78,8 +77,7 @@ client.on('message', message => {
                 })
             })
             return message.channel.send('Locked all channels');
-        } else if (args[1] === 'off') {
-            channels.forEach(channel => {
+        } else if (args[1] === 'off') => {
                 channel.updateOverwrite(message.guild.roles.everyone, {
                     SEND_MESSAGES: true
                 }).then(() => {
