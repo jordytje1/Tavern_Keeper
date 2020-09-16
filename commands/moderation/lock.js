dec1df74a4b670114108a3cd4177cb3b85e2099c
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-16 19:42:56 +0200

    Create lock.js

---
+++ commands/moderation/lock.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'lock',
+	category: 'moderation',
+	description: 'Feeling bored? Get some activities to do.',
+	aliases: [],
+	usage: 'lock',
+	run: async (client, message, args) => {
+    if(!message.content.startsWith('!add'))return;  
+
+  let channelsend = new Discord.MessageEmbed()
+  .setColor('#e64b0e')
+  .setTitle(`Added User`)
+  .setDescription(`${message.author} Has locked this channel`)
+
+  message.channel.overwritePermissions([
+  {
+     id: message.guild.roles.everyone,
+     deny: ['SEND_MESSAGES', 'ADD_REACTIONS'],
+                    }).then(() => {
+                    channel.setName(channel.name += `🔒`)
+  },
+]);
+  message.channel.send(channelsend)
+
+    
+    }  
+  }
