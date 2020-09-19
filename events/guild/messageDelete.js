c4034e25adeafec78a91a51aaf3ef82a2b9b24aa
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-19 14:07:13 +0200

    Create messageDelete.js

---
+++ events/guild/messageDelete.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const moment = require("moment");
+
+exports.name = 'messageDelete';
+exports.run = (client, message) => {
+
+    if (message.author.bot) return; 
+
+    if (!message || !message.id || !message.content || !message.guild) return;
+    const channel = message.guild.channels.find(channel => channel.name === "logs");
+    if (!channel) return;
+    channel.send(`\`[${moment(new Date()).format('h:mm:ss')}]\` 🗑 ${message.author.tag} (\`${message.author.id}\`) Mensagem deletada em **#${message.channel.name}**:\n${message.cleanContent}`);
+}
