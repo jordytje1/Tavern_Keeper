4b2252c69ace610304aa1c1bf56dd4d5b77ee056
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-19 16:38:23 +0200

    Update add.js

--- commands/moderation/add.js
+++ commands/moderation/add.js
@@ -6,7 +6,7 @@ module.exports = {
 	description: 'Feeling bored? Get some activities to do.',
 	aliases: [],
 	usage: 'add',
-	run: async (client, message, args) => {
+	run: async (client, message, args, first) => {
      let userrr = message.mentions.members.first()
     if(!message.content.startsWith('!add'))return;  
 
