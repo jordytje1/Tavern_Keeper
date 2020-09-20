7622b89a7cd703922b35d1c805cd75b34d70ff2d
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-20 14:48:45 +0200

    Update fly.js

--- commands/image/fly.js
+++ commands/image/fly.js
@@ -1,11 +1,25 @@
 /* eslint-disable no-unused-vars */
+const cooldowns = new Discord.Collection();
 module.exports = {
 	name: 'fly',
 	category: 'Image',
 	description: 'Sends a fake image of a fly that looks suspiciously real.',
 	aliases: [],
+	cooldown: 5,
 	usage: 'fly',
 	run: (client, message, args) => {
+		if (!cooldowns.has(command.name)) {
+	cooldowns.set(command.name, new Discord.Collection());
+}
+
+const now = Date.now();
+const timestamps = cooldowns.get(command.name);
+const cooldownAmount = (command.cooldown || 3) * 1000;
+
+if (timestamps.has(message.author.id)) {
+	// ...
+}
+
 		message.channel.send({ files: ['./assets/image/fly.png'] }).then(message.delete());
 	},
 };
