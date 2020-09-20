622d1c71bf42934c02170e21428b174f4d9f5a35
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-20 20:58:39 +0200

    Update lock.js

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -4,12 +4,16 @@ module.exports = {
     description: "lock channel.",
     usage: "lock ",
     run: (client, message, args, level) => {
+      let user = message.guild.id
   try {
     message.channel.send('locking Channel...');
     
-    message.channel.overwritePermissions(message.guild.id, {
-      SEND_MESSAGES: false
-    });
+    message.channel.overwritePermissions([
+        {
+      id: user,
+      deny: ['SEND_MESSAGES'],
+    },
+        ]);
   } catch (err) {
     message.channel.send('There was an error!\n' + err).catch();
 };
