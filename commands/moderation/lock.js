605d4172ec37b0037347809903d7a04dd004debd
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2020-09-20 20:47:26 +0200

    Create lock.js

---
+++ commands/moderation/lock.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "lock",
+    category: 'moderation',
+    description: "lock channel.",
+    usage: "lock ",
+    run: (client, message, args, level) => {
+  try {
+    await message.channel.send('locking Channel...');
+    
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    });
+  } catch (err) {
+    message.channel.send('There was an error!\n' + err).catch();
+};
+}
+}
